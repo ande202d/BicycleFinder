dee7f2b96a9e8edc4e40d70ac1ed76121ebac620
ande202d <48085313+ande202d@users.noreply.github.com>
2020-11-02 14:37:02 +0100

    added Test, and got Accepted by ANDERS

--- app/src/androidTest/java/com/example/bicyclefinder/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/bicyclefinder/ExampleInstrumentedTest.java
@@ -4,10 +4,24 @@ import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -17,10 +31,25 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.bicyclefinder", appContext.getPackageName());
+
+        onView(withId(R.id.mainEditTextLoginEmail)).perform(typeText("dav"));
+        onView(withId(R.id.mainEditTextLoginPassword)).perform(typeText("dav"));
+        onView(withId(R.id.mainButtonLogin)).perform(click());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withId(R.id.mainTextViewMessage)).check(matches(withText("Ugyldigt login")));
     }
 }
